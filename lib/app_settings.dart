c3406be2d36b0598d9809460a23318c262ab7bdd
zkhan <apps4av@gmail.com>
2024-05-27 18:12:32 -0400

    removed misbehaving package settings_screen_ex

--- lib/app_settings.dart
+++ lib/app_settings.dart
@@ -2,194 +2,195 @@
 import 'package:avaremp/chart.dart';
 import 'package:avaremp/documents_screen.dart';
 import 'package:avaremp/settings_cache_provider.dart';
-import 'package:flutter_settings_screen_ex/flutter_settings_screen_ex.dart';
 
 class AppSettings {
 
+  late final SettingsCacheProvider provider;
+
   Future<void> initSettings() async {
-    await Settings.init(
-      cacheProvider: SettingsCacheProvider(),
-    );
+    provider = SettingsCacheProvider();
+    provider.init();
   }
 
   void setChartType(String chart) {
-    Settings.setValue("key-chart-v1", chart);
+    provider.setString("key-chart-v1", chart);
   }
 
   String getChartType() {
-    return Settings.getValue("key-chart-v1", defaultValue: ChartCategory.sectional) as String;
+    return provider.getValue("key-chart-v1", defaultValue: ChartCategory.sectional) as String;
   }
 
   bool getNorthUp() {
-    return Settings.getValue("key-north-up", defaultValue: true) as bool;
+    return provider.getValue("key-north-up", defaultValue: true) as bool;
   }
 
   void setNorthUp(bool northUp) {
-    Settings.setValue("key-north-up", northUp);
+    provider.setBool("key-north-up", northUp);
   }
 
   void setZoom(double zoom) {
-    Settings.setValue("key-chart-zoom", zoom);
+    provider.setDouble("key-chart-zoom", zoom);
   }
 
   double getZoom() {
-    return Settings.getValue("key-chart-zoom", defaultValue: 0.0) as double;
+    return provider.getValue("key-chart-zoom", defaultValue: 0.0) as double;
   }
 
   void setRotation(double rotation) {
-    Settings.setValue("key-chart-rotation", rotation);
+    provider.setDouble("key-chart-rotation", rotation);
   }
 
   double getRotation() {
-    return Settings.getValue("key-chart-rotation", defaultValue: 0.0) as double;
+    return provider.getValue("key-chart-rotation", defaultValue: 0.0) as double;
   }
 
   void setCenterLatitude(double l) {
-    Settings.setValue("key-chart-center-latitude", l);
+    provider.setDouble("key-chart-center-latitude", l);
   }
 
   double getCenterLatitude() {
-    return Settings.getValue("key-chart-center-latitude", defaultValue: 37.0) as double;
+    return provider.getValue("key-chart-center-latitude", defaultValue: 37.0) as double;
   }
 
   void setCenterLongitude(double l) {
-    Settings.setValue("key-chart-center-longitude", l);
+    provider.setDouble("key-chart-center-longitude", l);
   }
 
   double getCenterLongitude() {
-    return Settings.getValue("key-chart-center-longitude", defaultValue: -95.0) as double;
+    return provider.getValue("key-chart-center-longitude", defaultValue: -95.0) as double;
   }
 
   void setInstruments(String instruments) {
-    Settings.setValue("key-instruments-v9", instruments);
+    provider.setString("key-instruments-v9", instruments);
   }
 
   String getInstruments() {
-    return Settings.getValue("key-instruments-v9", defaultValue: "GS,ALT,MT,NXT,DIS,BRG,ETA,ETE,UPT,UTC,SRC") as String;
+    return provider.getValue("key-instruments-v9", defaultValue: "GS,ALT,MT,NXT,DIS,BRG,ETA,ETE,UPT,UTC,SRC") as String;
   }
 
   List<String> getLayers() {
-    return (Settings.getValue("key-layers-v32", defaultValue: "Nav,Chart,OSM,Weather,NOAA-Loop,TFR,Plate,Traffic,PFD,Tracks") as String).split(",");
+    return (provider.getValue("key-layers-v32", defaultValue: "Nav,Chart,OSM,Weather,NOAA-Loop,TFR,Plate,Traffic,PFD,Tracks") as String).split(",");
   }
 
   List<bool> getLayersState() {
-    return (Settings.getValue("key-layers-state-v32", defaultValue: "true,true,true,false,false,true,false,true,false,false") as String).split(",").map((String e) => e == 'true' ? true : false).toList();
+    return (provider.getValue("key-layers-state-v32", defaultValue: "true,true,true,false,false,true,false,true,false,false") as String).split(",").map((String e) => e == 'true' ? true : false).toList();
   }
 
   void setLayersState(List<bool> state) {
-    Settings.setValue("key-layers-state-v32", state.map((bool e) => e.toString()).toList().join(","));
+    provider.setString("key-layers-state-v32", state.map((bool e) => e.toString()).toList().join(","));
   }
 
   void setCurrentPlateAirport(String name) {
-    Settings.setValue("key-current-plate-airport", name);
+    provider.setString("key-current-plate-airport", name);
   }
 
   String getCurrentPlateAirport() {
-    return Settings.getValue("key-current-plate-airport", defaultValue: "") as String;
+    return provider.getValue("key-current-plate-airport", defaultValue: "") as String;
   }
 
   void setEmail(String name) {
-    Settings.setValue("key-user-email", name);
+    provider.setString("key-user-email", name);
   }
 
   String getEmail() {
-    return Settings.getValue("key-user-email", defaultValue: "") as String;
+    return provider.getValue("key-user-email", defaultValue: "") as String;
   }
 
   void setPasswordBackup(String password) {
-    Settings.setValue("key-user-password-backup", password);
+    provider.setString("key-user-password-backup", password);
   }
 
   String getPasswordBackup() {
-    return Settings.getValue("key-user-password-backup", defaultValue: "") as String;
+    return provider.getValue("key-user-password-backup", defaultValue: "") as String;
   }
 
   void setEmailBackup(String name) {
-    Settings.setValue("key-user-email-backup", name);
+    provider.setString("key-user-email-backup", name);
   }
 
   String getEmailBackup() {
-    return Settings.getValue("key-user-email-backup", defaultValue: "") as String;
+    return provider.getValue("key-user-email-backup", defaultValue: "") as String;
   }
 
   void setLastRouteEntry(String value) {
-    Settings.setValue("key-last-route-entry", value);
+    provider.setString("key-last-route-entry", value);
   }
 
   String getLastRouteEntry() {
-    return Settings.getValue("key-last-route-entry", defaultValue: "") as String;
+    return provider.getValue("key-last-route-entry", defaultValue: "") as String;
   }
 
   void setAircraft(String name) {
-    Settings.setValue("key-user-aircraft", name);
+    provider.setString("key-user-aircraft", name);
   }
 
   String getAircraft() {
-    return Settings.getValue("key-user-aircraft", defaultValue: "") as String;
+    return provider.getValue("key-user-aircraft", defaultValue: "") as String;
   }
 
   void setChecklist(String name) {
-    Settings.setValue("key-user-checklist", name);
+    provider.setString("key-user-checklist", name);
   }
 
   String getChecklist() {
-    return Settings.getValue("key-user-checklist", defaultValue: "") as String;
+    return provider.getValue("key-user-checklist", defaultValue: "") as String;
   }
 
   int getTas() {
-    return Settings.getValue("key-airplane-tas-v2", defaultValue: 100) as int;
+    return provider.getValue("key-airplane-tas-v2", defaultValue: 100) as int;
   }
 
   void setTas(int value) {
-    Settings.setValue("key-airplane-tas-v2", value);
+    provider.setInt("key-airplane-tas-v2", value);
   }
 
   int getFuelBurn() {
-    return Settings.getValue("key-airplane-fuel-burn-v2", defaultValue: 10) as int;
+    return provider.getValue("key-airplane-fuel-burn-v2", defaultValue: 10) as int;
   }
 
   void setFuelBurn(int value) {
-    Settings.setValue("key-airplane-fuel-burn-v2", value);
+    provider.setInt("key-airplane-fuel-burn-v2", value);
   }
 
   bool isSigned() {
-    return Settings.getValue("key-signed", defaultValue: false) as bool;
+    return provider.getValue("key-signed", defaultValue: false) as bool;
   }
 
   void setSign(bool value) {
-    Settings.setValue("key-signed", value);
+    provider.setBool("key-signed", value);
   }
 
   bool showIntro() {
-    return Settings.getValue("key-intro", defaultValue: true) as bool;
+    return provider.getValue("key-intro", defaultValue: true) as bool;
   }
 
   void setIntro(bool value) {
-    Settings.setValue("key-intro", value);
+    provider.setBool("key-intro", value);
   }
 
   void setDocumentPage(String name) {
-    Settings.setValue("key-document-page-v1", name);
+    provider.setString("key-document-page-v1", name);
   }
 
   String getDocumentPage() {
-    return Settings.getValue("key-document-page-v1", defaultValue: DocumentsScreen.allDocuments) as String;
+    return provider.getValue("key-document-page-v1", defaultValue: DocumentsScreen.allDocuments) as String;
   }
 
   bool isAudibleAlertsEnabled() {
-    return Settings.getValue("key-audible-alerts", defaultValue: true) as bool;
+    return provider.getValue("key-audible-alerts", defaultValue: true) as bool;
   }
 
   void setAudibleAlertsEnabled(bool value) {
-    Settings.setValue("key-audible-alerts", value);
+    provider.setBool("key-audible-alerts", value);
   }
 
   bool isInstrumentsVisiblePlate() {
-    return Settings.getValue("key-enable-instruments-plate", defaultValue: true) as bool;
+    return provider.getValue("key-enable-instruments-plate", defaultValue: true) as bool;
   }
 
   void setInstrumentsVisiblePlate(bool value) {
-    Settings.setValue("key-enable-instruments-plate", value);
+    provider.setBool("key-enable-instruments-plate", value);
   }
 
 }
+
--- lib/settings_cache_provider.dart
+++ lib/settings_cache_provider.dart
@@ -1,47 +1,39 @@
 import 'dart:async';
-import 'package:flutter_settings_screen_ex/flutter_settings_screen_ex.dart';
 import 'data/user_settings_realm_helper.dart';
 
-class SettingsCacheProvider extends SharePreferenceCache {
+class SettingsCacheProvider {
 
   final UserSettingsRealmHelper _userSettingsRealmHelper = UserSettingsRealmHelper();
 
-  @override
   bool containsKey(String key) {
     bool contains = getKeys().contains(key);
     return contains;
   }
 
-  @override
   bool? getBool(String key, {bool? defaultValue}) {
     String? value = _userSettingsRealmHelper.getSetting(key);
     return value == null ? defaultValue : value == "false" ? false : true;
   }
 
-  @override
   double? getDouble(String key, {double? defaultValue}) {
     String? value = _userSettingsRealmHelper.getSetting(key);
     return value == null ? defaultValue : double.parse(value);
   }
 
-  @override
   int? getInt(String key, {int? defaultValue}) {
     String? value = _userSettingsRealmHelper.getSetting(key);
     return value == null ? defaultValue : int.parse(value);
   }
 
-  @override
   Set getKeys() {
     return _userSettingsRealmHelper.getAllSettings().map((e) => e['key']).toSet();
   }
 
-  @override
   String? getString(String key, {String? defaultValue}) {
     String? value = _userSettingsRealmHelper.getSetting(key);
     return value ?? defaultValue;
   }
 
-  @override
   T? getValue<T>(String key, {T? defaultValue}) {
 
     if(T == double) {
@@ -59,48 +51,40 @@ class SettingsCacheProvider extends SharePreferenceCache {
     return defaultValue;
   }
 
-  @override
   Future<void> init() async {
     _userSettingsRealmHelper.init();
   }
 
-  @override
   Future<void> remove(String key) {
     _userSettingsRealmHelper.deleteSetting(key);
     return Future.value();
   }
 
-  @override
   Future<void> removeAll() {
     _userSettingsRealmHelper.deleteAllSettings();
     return Future.value();
   }
 
-  @override
   Future<void> setBool(String key, bool? value) {
     _userSettingsRealmHelper.insertSetting(key, value.toString());
     return Future.value();
   }
 
-  @override
   Future<void> setDouble(String key, double? value) {
     _userSettingsRealmHelper.insertSetting(key, value.toString());
     return Future.value();
   }
 
-  @override
   Future<void> setInt(String key, int? value) {
     _userSettingsRealmHelper.insertSetting(key, value.toString());
     return Future.value();
   }
 
-  @override
   Future<void> setObject<T>(String key, T? value) {
     _userSettingsRealmHelper.insertSetting(key, value.toString());
     return Future.value();
   }
 
-  @override
   Future<void> setString(String key, String? value) {
     _userSettingsRealmHelper.insertSetting(key, value.toString());
     return Future.value();
