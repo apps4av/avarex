24289b06de95ff97104e2f5e2d88fae43d5b6185
zkhan <apps4av@gmail.com>
2024-12-24 09:30:53 -0500

    mock GPS for testing

--- lib/gps.dart
+++ lib/gps.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:math';
 
+import 'package:avaremp/geo_calculations.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 
@@ -62,6 +64,39 @@ class Gps {
     }
   }
 
+  // for testing
+  StreamSubscription<Position> getStreamMock() {
+    final List<LatLng> mockPositions = GeoCalculations().findPoints(const LatLng(42.584, -70.916), const LatLng(42.584 + 0.75, -70.916), 1);
+    mockPositions.addAll(mockPositions.reversed.toList()); // come back
+    return Stream.periodic(const Duration(seconds: 1), (_) {
+      double count = DateTime.now().second.toDouble();
+      // change speed to simulate takeoff and landing, to 50 m/s, and one cycle in 180 seconds
+      double speed = sin(((count * 2) % 360) * pi / 180) * 50;
+      if(speed < 0) {
+        speed = 0;
+      }
+
+      // change altitude to simulate takeoff and landing, to 1000 meters, and one cycle in 180 seconds
+      double altitude = sin(((count * 2) % 360) * pi / 180) * 1000;
+      if(altitude < 0) {
+        altitude = 0;
+      }
+
+      // Code returning a value every 1 seconds.
+      return Position(
+        longitude: mockPositions[count.toInt() % mockPositions.length].longitude,
+        latitude: mockPositions[count.toInt() % mockPositions.length].latitude,
+        accuracy: 0,
+        altitude: altitude,
+        altitudeAccuracy: 0,
+        heading: count % 90,
+        headingAccuracy: 0,
+        speed: speed,
+        speedAccuracy: 0,
+        timestamp: DateTime.now(),
+      );
+    }).listen((Position position) {});
+  }
 
   StreamSubscription<Position> getStream() {
 
