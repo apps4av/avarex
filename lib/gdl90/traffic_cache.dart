5f274675e2877abdc30932aaef0310d756d0b733
Shane Lenagh <shanelenagh@gmail.com>
2025-03-16 13:09:42 -0500

    Fixiing a bug with callsigns with large characters (e.g., W) in non-monospaced fonts not showing up in display on Windows

--- lib/gdl90/traffic_cache.dart
+++ lib/gdl90/traffic_cache.dart
@@ -579,7 +579,7 @@ class TrafficIdPainter extends AbstractCachedCustomPainter {
   static final _boundingBoxPaint = Paint()..color = const Color.fromRGBO(0, 0, 0, .2);
   static const _trafficIdTextStyle = TextStyle(shadows: [Shadow(offset: Offset(2, 2))], color: Colors.white, fontWeight: FontWeight.w600, fontSize: _trafficIdFontSize);
   static const double _offsetX = 24, _offsetY = 24;
-  static const double _charPixeslWidth = 11;
+  static const double _charPixeslWidth = 12;
 
   final String _trafficId;
   final bool _isAirborne;
