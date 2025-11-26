9d152bee3f0fc0163084063386fcda6bfe4491d1
Bradley Spatz <bradley.spatz@gmail.com>
2025-11-26 00:40:59 -0500

    Update notam_cache.dart
    Use working FAA NOTAM API based on https://notams.aim.faa.gov/notamSearch/ which returns JSON that we parse.  Include LTA (Letters to Airmen) but could exclude or enhance to hyperlink to LTA URL.

--- lib/weather/notam_cache.dart
+++ lib/weather/notam_cache.dart
@@ -1,5 +1,5 @@
 import 'dart:typed_data';
-import 'package:avaremp/app_log.dart';
+import 'dart:convert';
 import 'package:avaremp/constants.dart';
 import 'package:avaremp/data/main_database_helper.dart';
 import 'package:avaremp/data/weather_database_helper.dart';
@@ -7,7 +7,6 @@ import 'package:avaremp/destination/destination.dart';
 import 'package:avaremp/storage.dart';
 import 'package:avaremp/weather/weather.dart';
 import 'package:avaremp/weather/weather_cache.dart';
-import 'package:html/parser.dart' as html show parse;
 import 'package:http/http.dart' as http;
 
 import 'notam.dart';
@@ -18,72 +17,35 @@ class NotamCache extends WeatherCache {
 
   @override
   Future<void> parse(List<Uint8List> data, [String? argument]) async {
-
     if(null == argument) {
       return;
     }
 
-    String? cookie;
-    //@@___asa_password___@@
-    final responseHttp = await http.post(Uri.parse("https://rfinder.asalink.net/login.php?cmd=login&uid=apps4av&pwd=@@___asa_password__@@"));
-    if (responseHttp.statusCode == 200) {
-      try {
-        cookie = responseHttp.headers['set-cookie'];
+    // Store results
+    List<String> allNotams = [];
+
+    // JSON returned from Internet NOTAM API; grok it
+    for (var notam in jsonDecode(utf8.decode(data[0]))['notamList']) {
+      var text = notam['traditionalMessage'].replaceAll('\n', '');
+      if (notam['featureName'] == "LTA") {  // Letters to Airmen: include?  hyperlink to reference?
+        //continue;
+        text = "[LTA] " + text + ", see " + notam['notamNumber'] + " (" + notam['comment'] + ")";
+      } else {
+        text = text.split(" ").sublist(2).join(" ");  // skip first 2 words, e.g. "!GNV 10/027"
       }
-      catch(e) {
-        AppLog.logMessage("ASA NOTAM cookie error $e");
-      }
+      allNotams.add(text);
     }
 
-    Map<String, String> headers = {};
-    headers['Cookie'] = cookie ?? "";
-    final response = await http.get(Uri.parse("https://rfinder.asalink.net/avionet_run.php?form_id=notam_inquiry&apt_icao_id=$argument"), headers: headers);
-    if (response.statusCode == 200) {
-      String data = response.body;
-
-      // Parse HTML
-      final document = html.parse(data);
-
-      // Find all <tt> elements
-      final ttElements = document.getElementsByTagName('tt');
-
-      // Store results
-      List<List<String>> allNotams = [];
-
-      for (var tt in ttElements) {
-        // Get raw HTML text of this <tt>
-        final ttHtml = tt.innerHtml;
-
-        // Regex to capture content between <b>E)</b> and <br>
-        final regex = RegExp(r'<b>E\)</b>\s*(.*?)<br>', dotAll: true);
-        final match = regex.firstMatch(ttHtml);
-
-        if (match != null) {
-          // Extracted text
-          String content = match.group(1) ?? '';
-
-          // Clean HTML tags if any remain
-          content = content.replaceAll(RegExp(r'<[^>]*>'), '').trim();
-
-          // Split into lines
-          List<String> lines = content.split('\n').map((e) => e.trim()).where((e) => e.isNotEmpty).toList();
-
-          allNotams.add(lines);
-        }
-      }
-
-      String all = "Unable to get NOTAMs.";
-      if(allNotams.isNotEmpty) {
-        all = allNotams.join("\n\n");
-      }
-      Notam notam = Notam(argument,
-          DateTime.now().toUtc().add(
-              const Duration(minutes: Constants.weatherUpdateTimeMin)),
-          DateTime.now().toUtc(),
-          Weather.sourceInternet, all);
-
-      await WeatherDatabaseHelper.db.addNotam(notam);
+    String all = "Unable to get NOTAMs.";
+    if(allNotams.isNotEmpty) {
+      all = allNotams.join("\n");
     }
+    Notam notam = Notam(argument,
+        DateTime.now().toUtc().add(
+            const Duration(minutes: Constants.weatherUpdateTimeMin)),
+        DateTime.now().toUtc(),
+        Weather.sourceInternet, all);
+    await WeatherDatabaseHelper.db.addNotam(notam);
   }
 
   // Download and parse, override because this is a POST
@@ -93,25 +55,23 @@ class NotamCache extends WeatherCache {
       return;
     }
 
-    // find lat/lon
     Destination? airport = await MainDatabaseHelper.db.findAirport(argument);
     if(null == airport) {
       return;
     }
 
-    String ll = Destination.toSexagesimal(airport.coordinate);
+    // find lat/lon
+    //String ll = Destination.toSexagesimal(airport.coordinate);
 
+    // 2025-11-25, bspatz: try NOTAMs via working FAA API, by locationID
+    String _icaoID = airport.locationID;
+    if (_icaoID.substring(0,1) == "K" && _icaoID.length == 4) { // more coverage
+      _icaoID = _icaoID.substring(1);
+    }
     Map<String, String> body = {
-      "geoLatDegree": ll.substring(0, 3),
-      "geoLatMinute": ll.substring(3, 5),
-      "geoLatNorthSouth": ll.substring(7, 8),
-      "geoLongDegree": ll.substring(9, 12),
-      "geoLongMinute": ll.substring(12, 14),
-      "geoLongEastWest": ll.substring(16, 17),
-      "reportType": "Raw",
-      "geoLatLongRadius": "20",
-      "actionType": "latLongSearch",
-      "submit": "View+NOTAMs",
+      "searchType": "0",
+      "sortColumns": "3 false", // column "Class" on notams.aim.faa.gov/notamSearch/nsapp.html
+      "designatorsForLocation": _icaoID,
     };
 
     try {
@@ -137,4 +97,3 @@ class NotamCache extends WeatherCache {
     return w;
   }
 }
-
